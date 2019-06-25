4e36e9636fa13aae01ff52a4d2b45e60f7a29df1
Iyaad Rafiq <webdev@iyaad.com>
2019-06-25 12:53:59 +0100

    Update files

--- js/calculator.js
+++ js/calculator.js
@@ -13,38 +13,6 @@ var calculator = {
 
 
 
-/***********
-ERRORS
-
-//7.8 - 6 = 1.7999998
-//n operator n operator = 
-
-//infinity operator number =
-
-
-****************/
-
-
-/***********
-SEQUENCES
-
-
-//= y
-// = number etc.  y
-//number = y
-//number = number etc. y
-
-
-
-//only problem now: = operator
-
-//= operator n = n = concatenate
-//n = operator n
-//n = operator = operator n
-
-****************/
-
-
 
 var result;
 var calcStr;
@@ -188,10 +156,6 @@ function opClicked(btnVal) {
       curNum = 1;
     }
     
-  /*
-  
-  
-  */
 
   operator = btnVal;
 
@@ -248,111 +212,3 @@ function eqClicked() {
   numberClicked = false;
   
 } 
-
-
-
-
-
-
-
-/*
-
-
-else if (num[0] && !num[1]) {
-    
-    num[0] = Number(num[0]);
-    num[1] = Number(num[1]);
-
-    result = calculate(num[0], num[1], operator);
-    DOM.input.value = result;
-    num[0] = result.toString();  
-    num[1] = 0;
-    operator = '';
-    
-  } else if (!num[0]) {
-    
-    num[0] = Number(num[0]);
-    num[1] = Number(num[1]);
-
-    result = calculate(num[0], num[1], operator);
-    DOM.input.value = result;
-    num[0] = result.toString();  
-    num[1] = 0;
-    operator = '';
-    
-  } 
-
-
-
-*/
-
-
-
-/*else if (firstValue && !secondValue) {
-    
-    
-    num[0] = Number(num[0]);
-    num[1] = Number(num[1]);
-    
-    result = num[0];
-    DOM.input.value = result;
-    num[0] = result;
-    num[1] = '0';
-    firstValue = true;
-    secondValue = false;
-  }
-  */
-
-
-//1 + 1 = 2          
-//ans + 2 = 4
-
-//init onload and on button
-
-//click button
-
-//is button number, dot, equals or operator
-
-//if number - 
-
-   //add num to numStr
-   //num1 or num2?
-   //get and store in num1 / num2 vars
-   //display num on screen
-
-//if operator
-   //
-   //store operator in var? (will replace operator already stored in var) - enables change operator if wrong operator
-   //if num1 exists
-     //current number will be num2
-   
-
-
-//if equals operator
-  //only do something if num1 and num2 exist?
-  //if num1 and num2 exist
-     //perform calculation
-         //if statement for each operator
-     //result = num1
-
-
-
-
-
-                     
-
-                     
-/*if (operatorClicked) {
-        console.log(calcStr);
-        indexSpace = calcStr.indexOf(' ');
-        calcStr = calcStr.slice(0, indexSpace);
-        console.log(calcStr);
-        
-        //text = text.slice(0, i) + "the  Second World War" + text.slice(i + 12);
-      }*/                     
-                  
-                    
-
-
-
-
